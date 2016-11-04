6c740b473dcdb0c44588ea819ac78376e269b245
Carlo Lobrano <carlo.lobrano@telit.com>
2016-11-04 12:58:22 +0100

    Fixed readme in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
 __version__ = '0.1.0'
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(name='letsdo',
       author='Carlo Lobrano',
