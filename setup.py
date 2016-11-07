3b68f2d65634f78bb04fb48c24d07f36e67e6776
Carlo Lobrano <carlo.lobrano@telit.com>
2016-11-07 14:25:32 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
