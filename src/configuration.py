0ca6331d18cb5ee4a242044797ccce7e3cb2fc7a
Carlo Lobrano <c.lobrano@gmail.com>
2017-08-12 16:30:42 +0200

    codereview: moved configuration code in dedicated file

---
+++ src/configuration.py
@@ -0,0 +1,50 @@
+import os
+import yaml
+from log import dbg
+
+class Configuration(object):
+    def __init__(self):
+        conf_file_path = os.path.expanduser(os.path.join('~', '.letsdo'))
+        if os.path.exists(conf_file_path):
+            self.configuration = yaml.load(open(conf_file_path).read())
+
+            self.data_dir = self.__get_value('DATADIR')
+            if self.data_dir:
+                self.data_fullpath = os.path.join(self.data_dir,
+                                                  '.letsdo-data')
+                self.task_fullpath = os.path.join(self.data_dir,
+                                                  '.letsdo-task')
+            else:
+                info('letsdo data will be saved into HOME directory')
+                self.data_fullpath = os.path.expanduser(
+                    os.path.join('~', '.letsdo-data'))
+                self.task_fullpath = os.path.expanduser(
+                    os.path.join('~', '.letsdo-task'))
+
+            if self.__get_value('TODO_FULLPATH'):
+                self.todo_fullpath = os.path.expanduser(
+                    self.__get_value('TODO_FULLPATH'))
+            else:
+                self.todo_fullpath = None
+
+            self.todo_start_tag = self.__get_value('TODO_START_TAG')
+            self.todo_stop_tag = self.__get_value('TODO_STOP_TAG')
+
+        else:
+            dbg('Config file not found. Using default')
+            self.data_fullpath = os.path.expanduser(
+                os.path.join('~', '.letsdo-data'))
+            self.task_fullpath = os.path.expanduser(
+                os.path.join('~', '.letsdo-task'))
+
+    def __get_value(self, key):
+        try:
+            value = os.path.expanduser(self.configuration[key])
+        except KeyError as e:
+            dbg('configuration: Could not find \'{key}\' field: {msg}'.format(
+                key=key, msg=e.message))
+            return None
+        return value
+
+
+
--- src/letsdo.py
+++ src/letsdo.py
@@ -22,63 +22,18 @@ import os
 import datetime
 import docopt
 import sys
-import yaml
 import re
 from string import Formatter
 import hashlib
 import json
 from terminaltables import SingleTable, AsciiTable
 from log import info, err, warn, dbg, do_color
+from configuration import Configuration
 
 def paint(msg):
     if msg and do_color:
         return raf.paint(str(msg))
     return msg
-
-class Configuration(object):
-    def __init__(self):
-        conf_file_path = os.path.expanduser(os.path.join('~', '.letsdo'))
-        if os.path.exists(conf_file_path):
-            self.configuration = yaml.load(open(conf_file_path).read())
-
-            self.data_dir = self.__get_value('DATADIR')
-            if self.data_dir:
-                self.data_fullpath = os.path.join(self.data_dir,
-                                                  '.letsdo-data')
-                self.task_fullpath = os.path.join(self.data_dir,
-                                                  '.letsdo-task')
-            else:
-                info('letsdo data will be saved into HOME directory')
-                self.data_fullpath = os.path.expanduser(
-                    os.path.join('~', '.letsdo-data'))
-                self.task_fullpath = os.path.expanduser(
-                    os.path.join('~', '.letsdo-task'))
-
-            if self.__get_value('TODO_FULLPATH'):
-                self.todo_fullpath = os.path.expanduser(
-                    self.__get_value('TODO_FULLPATH'))
-            else:
-                self.todo_fullpath = None
-
-            self.todo_start_tag = self.__get_value('TODO_START_TAG')
-            self.todo_stop_tag = self.__get_value('TODO_STOP_TAG')
-
-        else:
-            dbg('Config file not found. Using default')
-            self.data_fullpath = os.path.expanduser(
-                os.path.join('~', '.letsdo-data'))
-            self.task_fullpath = os.path.expanduser(
-                os.path.join('~', '.letsdo-task'))
-
-    def __get_value(self, key):
-        try:
-            value = os.path.expanduser(self.configuration[key])
-        except KeyError as e:
-            dbg('configuration: Could not find \'{key}\' field: {msg}'.format(
-                key=key, msg=e.message))
-            return None
-        return value
-
 
 class Task(object):
     def __init__(self, name='unknown', start=None, end=None, id=None):
