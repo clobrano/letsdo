96c58964c70768bf2936c298d4a0309efc1a9344
Carlo Lobrano <c.lobrano@gmail.com>
2018-06-13 07:47:40 +0200

    Added some minor NLP for time (today, yesterday)

--- src/timetoolkit.py
+++ src/timetoolkit.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 import re
 from string import Formatter
 
@@ -57,88 +57,94 @@ def format_h_m(string):
 
 
 def str2datetime(string):
+    if string == 'today':
+        return datetime.now()
+
+    if string == 'yesterday':
+        return = datetime.now() - timedelta(1)
+
     m = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        return datetime.datetime.strptime(string, '%Y-%m-%d %H:%M')
+        return datetime.strptime(string, '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}.\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        return datetime.datetime.strptime(string, '%Y-%m-%d %H.%M')
+        return datetime.strptime(string, '%Y-%m-%d %H.%M')
 
     m = re.findall(r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        return datetime.datetime.strptime(string, '%Y/%m/%d %H:%M')
+        return datetime.strptime(string, '%Y/%m/%d %H:%M')
 
     m = re.findall(r'\d{4}/\d{2}/\d{2} \d{2}.\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        return datetime.datetime.strptime(string, '%Y/%m/%d %H.%M')
+        return datetime.strptime(string, '%Y/%m/%d %H.%M')
 
     m = re.findall(r'\d{4}-\d{2}-\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        now_str = datetime.datetime.now().strftime('%H:%M')
-        return datetime.datetime.strptime(string + ' ' + now_str,
+        now_str = datetime.now().strftime('%H:%M')
+        return datetime.strptime(string + ' ' + now_str,
                                           '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{4}/\d{2}/\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        now_str = datetime.datetime.now().strftime('%H:%M')
-        return datetime.datetime.strptime(string + ' ' + now_str,
+        now_str = datetime.now().strftime('%H:%M')
+        return datetime.strptime(string + ' ' + now_str,
                                           '%Y/%m/%d %H:%M')
 
     m = re.findall(r'\d{2}-\d{2} \d{2}:\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        year_str = datetime.datetime.today().strftime('%Y')
-        return datetime.datetime.strptime(year_str + '-' + string,
+        year_str = datetime.today().strftime('%Y')
+        return datetime.strptime(year_str + '-' + string,
                                           '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{2}-\d{2} \d{2}.\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        year_str = datetime.datetime.today().strftime('%Y')
-        return datetime.datetime.strptime(year_str + '-' + string,
+        year_str = datetime.today().strftime('%Y')
+        return datetime.strptime(year_str + '-' + string,
                                           '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{2}-\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        year_str = datetime.datetime.today().strftime('%Y')
-        now_str = datetime.datetime.now().strftime('%H:%M')
-        return datetime.datetime.strptime(
+        year_str = datetime.today().strftime('%Y')
+        now_str = datetime.now().strftime('%H:%M')
+        return datetime.strptime(
             year_str + '-' + string + ' ' + now_str, '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{2}:\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        today_str = datetime.datetime.today().strftime('%Y-%m-%d')
-        return datetime.datetime.strptime(today_str + ' ' + string,
+        today_str = datetime.today().strftime('%Y-%m-%d')
+        return datetime.strptime(today_str + ' ' + string,
                                           '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d:\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        today_str = datetime.datetime.today().strftime('%Y-%m-%d')
-        return datetime.datetime.strptime(today_str + ' ' + string,
+        today_str = datetime.today().strftime('%Y-%m-%d')
+        return datetime.strptime(today_str + ' ' + string,
                                           '%Y-%m-%d %H:%M')
 
     m = re.findall(r'\d{2}.\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        today_str = datetime.datetime.today().strftime('%Y-%m-%d')
-        return datetime.datetime.strptime(today_str + ' ' + string,
+        today_str = datetime.today().strftime('%Y-%m-%d')
+        return datetime.strptime(today_str + ' ' + string,
                                           '%Y-%m-%d %H.%M')
 
     m = re.findall(r'\d.\d{2}', string)
     if len(m) != 0:
         string = m[0]
-        today_str = datetime.datetime.today().strftime('%Y-%m-%d')
-        return datetime.datetime.strptime(today_str + ' ' + string,
+        today_str = datetime.today().strftime('%Y-%m-%d')
+        return datetime.strptime(today_str + ' ' + string,
                                           '%Y-%m-%d %H.%M')
 
     raise ValueError('Date format not recognized: %s' % string)
