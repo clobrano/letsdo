cbf3dc1365d4a746fe325d846c5f6dba6af17c54
Carlo Lobrano <c.lobrano@gmail.com>
2023-04-02 22:18:21 +0200

    rework setup.py
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- setup.py
+++ setup.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+"""
 Setuptool configuration for letsdo
-'''
-import os
+"""
 from setuptools import setup, find_packages
 
-__version__ = '0.7.1'
+__version__ = "0.7.2"
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
 
-
-def read(fname):
-    '''Read a file in current directory'''
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-def long_description():
-    '''Check whether RST readme file exists to
-    read it, otherwise it looks for a Markdown one'''
-    if os.path.exists('README.rst'):
-        return read('README.rst')
-    return read('README.md')
-
-
-setup(name='letsdo',
-      author='Carlo Lobrano',
-      author_email='c.lobrano@gmail.com',
-      description='Time tracker for Command Line',
-      entry_points={'console_scripts': ['lets=letsdo:main', 'letsdo=letsdo:main']},
-      include_package_data=True,
-      install_requires=['docopt', 'PyYaml', 'terminaltables', 'parsedatetime', 'raffaello'],
-      keywords=['productivity', 'GTD', 'time tracker'],
-      license="MIT",
-      long_description=long_description(),
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      py_modules=['letsdo', 'log', 'configuration', 'timetoolkit'],
-      url='https://github.com/clobrano/letsdo',
-      version=__version__,
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Topic :: Utilities", ])
+setup(
+    name="letsdo",
+    version=__version__,
+    description="Time tracker for Command Line",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clobrano/letsdo",
+    author="Carlo Lobrano",
+    author_email="c.lobrano@gmail.com",
+    license="GPL3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+    ],
+    install_requires=[
+        "docopt",
+        "PyYaml",
+        "terminaltables",
+        "parsedatetime",
+        "raffaello",
+    ],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    entry_points={"console_scripts": ["lets=letsdo:main", "letsdo=letsdo:main"]},
+    include_package_data=True,
+    keywords=["productivity", "GTD", "time tracker"],
+    py_modules=["letsdo", "log", "configuration", "timetoolkit"],
+)
